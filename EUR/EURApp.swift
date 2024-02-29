ccb3cca340c25618e4fb41109066437e31d09f91
Gianmarco <gimmy03@icloud.com>
2024-02-29 19:11:44 +0100

    Initial Commit

---
+++ EUR/EURApp.swift
@@ -0,0 +1,17 @@
+//
+//  EURApp.swift
+//  EUR
+//
+//  Created by Gianmarco on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct EURApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
