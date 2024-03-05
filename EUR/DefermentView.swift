7196c0c4491dba773c7076d04f99427bb36249f4
Gianmarco <gimmy03@icloud.com>
2024-03-05 22:58:15 +0100

    add DefermentView

---
+++ EUR/DefermentView.swift
@@ -0,0 +1,20 @@
+//
+//  DefermentView.swift
+//  EUR
+//
+//  Created by Gianmarco on 02/03/24.
+//
+
+import SwiftUI
+
+struct DefermentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DefermentView_Previews: PreviewProvider {
+    static var previews: some View {
+        DefermentView()
+    }
+}
