c78bf4a38961cb6fd91cb067b6ad5d3d93393864
Gianmarco <gimmy03@icloud.com>
2024-03-05 22:59:38 +0100

    update PrincepsView

---
+++ EUR/PrincepsView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  EUR
+//
+//  Created by Gianmarco on 29/02/24.
+//
+
+import SwiftUI
+
+struct PrincepsView: View {
+    
+    var body: some View {
+        TabView {
+            SummaryView()
+                .tabItem{
+                    Label("Summary", systemImage: "list.bullet.rectangle")
+                }
+            DefermentView()
+                .tabItem{
+                    Label("Deferment", systemImage: "person.2.fill")
+                }
+            AccountView()
+                .tabItem {
+                    Label("Account", systemImage: "person.circle")
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        PrincepsView()
+    }
+}
