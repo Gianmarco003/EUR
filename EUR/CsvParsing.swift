fb41e20eba0115a11fb186a9aa5f195124ec19ec
Gianmarco <gimmy03@icloud.com>
2024-03-17 15:14:46 +0100

    add CSVParsing func

---
+++ EUR/CsvParsing.swift
@@ -0,0 +1,43 @@
+//
+//  CsvParsing.swift
+//  EUR
+//
+//  Created by Gianmarco on 17/03/24.
+//
+
+import SwiftUI
+
+struct ColumnsName {
+    var description = ""
+    var price = ""
+    var date = ""
+    var category = ""
+    
+    init(raw: [String]) {
+        description = raw[0]
+        price = raw[1]
+        date = raw[2]
+        category = raw[3]
+    }
+}
+
+func CSVParsing (CSV: String) -> [ColumnsName] {
+
+    var CSVToStruct = [ColumnsName]()
+    
+    //split the long string into an array of "rows" of data. Each is a string
+    //detect "\n" carriage return, then split
+    var rows = CSV.components(separatedBy: "\n")
+    
+    //remove header rows
+    rows.removeFirst()
+    
+    //now loop around each row and split into columns
+    for row in rows {
+        let CSVColumns = row.description.components(separatedBy: ";")
+        let CSVStruct = ColumnsName.init(raw: CSVColumns)
+        CSVToStruct.append(CSVStruct)
+    }
+    
+    return CSVToStruct
+}
