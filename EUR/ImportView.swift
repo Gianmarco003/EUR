c5e0b31c12bd5d2722e05e825030ed7e9934ab41
Gianmarco <gimmy03@icloud.com>
2024-03-17 15:17:42 +0100

    update ImportView with full import

--- EUR/ImportView.swift
+++ EUR/ImportView.swift
@@ -7,30 +7,76 @@
 
 import SwiftUI
 import TabularData
+import UniformTypeIdentifiers
 
 struct ImportView: View {
     
     @State private var importing = false
+    @State private var exporting = false
+    @StateObject var AppStorage = PermanentStorage()
+    
     
     var body: some View {
-        Button("Import") {
-            importing = true
-        }
-        .fileImporter(
-            isPresented: $importing,
-            allowedContentTypes: [.commaSeparatedText]
-        ) { result in
-            switch result {
-            case .success(let file):
-                let data = try? String(contentsOf: file)
-                print(result)
-                
-                
-                
-            case .failure(_):
-                print("Error while importing!")
+        VStack {
+            Button("Import") {
+                importing = true
             }
-        }    }
+            .buttonStyle(.bordered)
+            .fileImporter(
+                isPresented: $importing,
+                allowedContentTypes: [.commaSeparatedText]
+            ) { result in
+                switch result {
+                case .success(let file):
+                    let data = try? String(contentsOf: file)
+                    print("------------------------------------")
+                    print(result)
+                    print("------------------------------------")
+                    
+                    let FILE = CSVParsing(CSV: data!)
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "dd/mm/yyyy"
+                    for row in FILE {
+                        if !AppStorage.getCategories().contains(row.category) {
+                            AppStorage.addCategory(NewCategory: row.category)
+                        }
+                        AppStorage.addExpense(Expense(description: row.description,
+                                                      price: Double(row.price)!,
+                                                      date: dateFormatter.date(from: row.date)!,
+                                                      category: row.category))
+                        print(row)
+                    }
+                    
+                    
+                case .failure(_):
+                    print("Error while importing!")
+                }
+            }
+            Button("Export") {
+                exporting = true
+                var CSVString = "Description;Price;Date;Categories\n"
+                var CSVFile: Data = CSVString
+                
+            }
+            .buttonStyle(.bordered)
+            .fileExporter(isPresented: $exporting, documents: , contentType: .commaSeparatedText) {
+                
+            }
+            Button {
+                AppStorage.deleteCategories()
+            } label: {
+                Text("Delete categories")
+            }
+            .buttonStyle(.bordered)
+            Button {
+                AppStorage.deleteAllExpenses()
+            } label: {
+                Text("Delete expenses")
+            }
+            .buttonStyle(.bordered)
+            
+        }
+    }
 }
 
 struct ImportView_Previews: PreviewProvider {
