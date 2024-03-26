9d9934df557c71a84e633ff15ab41a847aa0a214
Gianmarco <gimmy03@icloud.com>
2024-03-26 20:08:41 +0100

    update ImportView

--- EUR/ImportView.swift
+++ EUR/ImportView.swift
@@ -13,7 +13,8 @@ struct ImportView: View {
     
     @State private var importing = false
     @State private var exporting = false
-    @StateObject var AppStorage = PermanentStorage()    
+    @StateObject var AppStorage = PermanentStorage()
+    @State var text = ""
     
     var body: some View {
         List {
@@ -26,12 +27,15 @@ struct ImportView: View {
             ) { result in
                 switch result {
                 case .success(let file):
-                    let data = try? String(contentsOf: file)
-                    print("------------------------------------")
-                    print(result)
-                    print("------------------------------------")
+                    let result = read(from: file)
+                    switch result {
+                    case .success(let text):
+                        self.text += text
+                    case .failure(let error):
+                        print(error)
+                    }
                     
-                    let FILE = CSVParsing(CSV: data!)
+                    let FILE = CSVParsing(CSV: text)
                     let dateFormatter = DateFormatter()
                     dateFormatter.dateFormat = "dd/mm/yyyy"
                     for row in FILE {
@@ -39,27 +43,26 @@ struct ImportView: View {
                             AppStorage.addCategory(NewCategory: row.category)
                         }
                         AppStorage.addExpense(Expense(description: row.description,
-                                                      price: Double(row.price)!,
+                                                      price: Double(row.price.replacingOccurrences(of: ",", with: "."))!,
                                                       date: dateFormatter.date(from: row.date)!,
                                                       category: row.category))
                         print(row)
                     }
                     
-                    
                 case .failure(_):
                     print("Error while importing!")
                 }
             }
-//            Button("Export") {
-//                exporting = true
-//                var CSVString = "Description;Price;Date;Categories\n"
-//                var CSVFile: Data = CSVString
-//
-//            }
-//            .buttonStyle(.bordered)
-//            .fileExporter(isPresented: $exporting, documents: , contentType: .commaSeparatedText) {
-//
-//            }
+            //            Button("Export") {
+            //                exporting = true
+            //                var CSVString = "Description;Price;Date;Categories\n"
+            //                var CSVFile: Data = CSVString
+            //
+            //            }
+            //            .buttonStyle(.bordered)
+            //            .fileExporter(isPresented: $exporting, documents: , contentType: .commaSeparatedText) {
+            //
+            //            }
             Button {
                 AppStorage.deleteCategories()
             } label: {
