3afc7d5e79ed3abea4245b966c50f8b3f0da0836
Gianmarco <gimmy03@icloud.com>
2024-04-04 18:24:24 +0200

    update CalendarElements with getMonthNumber func

--- EUR/CalendarElements.swift
+++ EUR/CalendarElements.swift
@@ -38,6 +38,37 @@ func getMonthName (Month: Int) -> String {
     }
 }
 
+func getMonthNumber (Month: String) -> Int {
+    switch Month {
+    case "January":
+        return 1
+    case "February":
+        return 2
+    case "March":
+        return 3
+    case "April":
+        return 4
+    case "May":
+        return 5
+    case "June":
+        return 6
+    case "July":
+        return 7
+    case "August":
+        return 8
+    case "September":
+        return 9
+    case "October":
+        return 10
+    case "November":
+        return 11
+    case "December":
+        return 12
+    default:
+        return 0
+    }
+}
+
 public let months = ["January",
                      "February",
                      "March",
